1cff1668515f77d71ed9ff63dd102e995c7c65ee
tarun1git <tarunrajput7959@gmail.com>
2022-09-24 00:24:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+// cursor movement
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + 'px';
+    cursor.style.left = x + 'px';
+    cursor.style.display = 'block';
+
+    // stop animation
+    function mouseStopped () {
+        cursor.style.display = 'none';
+    }
+
+    // hide animation
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 10000);
+
+    // stop animation
+    document.addEventListener("mouseout", () => {
+        cursor.style.display = 'none';
+    });
+
+});
